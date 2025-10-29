581757c247f8868ced656f40ba5e64b272a85031
Huytrongbeou <huytronh4@gmail.com>
2025-10-29 10:05:23 +0700

    2

--- product/src/controllers/productController.js
+++ product/src/controllers/productController.js
@@ -91,22 +91,20 @@ class ProductController {
       res.status(500).json({ message: "Server error" });
     }
   }
-
-
-
   async getProductById(req,res){
-    try{
-      const {id} = req.params;
+    try {
+      const {id}= req.params;
       const products = await this.productService.getProductById(id);
-      if (!product){
-        return res.status(404).json({message:"Product not found"});
+      if (!products){
+        return res.status(404).json("Product not found");
       }
-      res.status(200).json(products)
-    } catch (error){
-      console.error(error);
+      res.status(200)
+    } catch (error) {
+      console.log.error(error);
       res.status(500).json({message:"Server Error"});
     }
   }
+
 
 }
 
