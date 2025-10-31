9e8c647ab2fc6df5d4c834c44863e7eb8ceae9ed
Huytrongbeou <huytronh4@gmail.com>
2025-10-31 14:46:51 +0700

    20

--- product/src/controllers/productController.js
+++ product/src/controllers/productController.js
@@ -8,7 +8,7 @@ class ProductController {
     this.getProducts = this.getProducts.bind(this);
     this.createOrder = this.createOrder.bind(this);
     this.getOrderById = this.getOrderById.bind(this);
-    
+
     this.getProductsById = this.getProductsById.bind(this);
   }
   async createProduct(req, res) {
@@ -94,21 +94,19 @@ class ProductController {
     }
   }
   
-  async getProductsById(req,res){
+  async getProducts(req, res) {
     try {
-      const {id} = req.params;
+      const {id}= req.params
       const products = await this.productService.getProductsById(id);
-      if (!product){
-        return res.status(404).json({message:"Not found"});
+      if (!products.success) {
+        return res.status(404).json({ message: "Not found" });
       }
-      return res.status(200).json(products);
+      res.status(200).json(products);
     } catch (error) {
-      console.log(error);
-      return res.status(500).json({message:"Error server"});
+      console.error(error);
+      res.status(500).json({ message: "Server error" });
     }
   }
-
-
 }
 
 module.exports = ProductController;
